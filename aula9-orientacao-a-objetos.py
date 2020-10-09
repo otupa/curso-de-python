e7ecf7ed14a39990fbb2c4b10e2deb361302ca88
MarianoTupa <72321903+MarianoTupa@users.noreply.github.com>
2020-10-09 01:14:51 -0300

    Aula 9 - Orientação a objetos

---
+++ aula9-orientacao-a-objetos.py
@@ -0,0 +1,25 @@
+'''
+Aula 9 - Orientação a objeto
+
+#class
+uma classe é uma descrição que abstrai um conjunto de objetos com características similares.
+para exportar uma classe, utioliza-se:
+
+    from aula9 import Pessoas
+
+no exemplo acima o comando from esta buscando o arquivo 'aula9' e importando
+de dentro dele a classe Pessoas
+'''
+
+from aula9 import Pessoas
+
+mariano = Pessoas('mariano', 18, 1.75) #DENTRO DAS ASPAS DEFINO OS VALORES QUE PARA AS VARIAVEIS EM PESSOAS
+
+print(mariano)
+print('Nome:', mariano.nome, '\nIdade:', mariano.idade, '\nTamanho:', mariano.tamanho)
+
+#ENTÃO POSSO CRIAR UM PACOTE DE CODIGOS E EXPORTALOS PARA DENTO DE OUTRO CODIGO, UTILIZANDO class
+
+
+
+
---
+++ aula9.py
@@ -0,0 +1,12 @@
+'''
+para criar uma classe utiliza-se o comando class
+neste arquivo criarei a classe Pessoas que sera utilizado na aula 9
+'''
+
+class Pessoas:
+    def __init__(self, nome, idade, tamanho):
+        self.nome = nome
+        self.idade = idade
+        self.tamanho = tamanho
+
+
