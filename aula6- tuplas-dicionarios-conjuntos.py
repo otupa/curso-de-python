832b0f9824f04ed0eac661ca73aed77630c7834e
MarianoTupa <72321903+MarianoTupa@users.noreply.github.com>
2020-10-09 00:42:06 -0300

    Aula 6 - Tuplas, dicionarios e conjuntos

---
+++ aula6- tuplas-dicionarios-conjuntos.py
@@ -0,0 +1,32 @@
+'''
+Aula 6 - tuplas, dicionarios e conjuntos
+'''
+
+minha_lista = ['mariano', 'joao',]     #LISTA (list)
+minha_tupla = ('mariano', 'joao')      #TUPLA (tuple)
+meu_dicionario = {'nome': 'guilherme', 'idade': 27}  #DICIONARIO (dict)
+meu_conjunto = {'mariano', 'joao'}     #conjunto
+
+#para abrir:
+lista = []
+tupla = ()
+dicionario = {}
+conjunto = set()
+
+lista.clear()
+'''
+#para perguntar se um objeto esta dentro da lista, tupla,
+
+if 'mariano' in minha_tupla:
+    print('mariano')
+'''
+
+#para perguntar um item que esta no dicionario
+'print(meu_dicionario['
+
+
+
+#para perguntar valores de um dicionario
+for valores in meu_dicionario.values():
+    print(valores)
+
